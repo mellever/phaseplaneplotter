3e72f9943cc46fa32f1b750784abe97c66156599
Melle <93864179+mellever@users.noreply.github.com>
2024-08-07 16:35:10 +0200

    update

--- ppplotter2.py
+++ ppplotter2.py
@@ -5,7 +5,7 @@ from PyQt6.QtWidgets import *
 
 import numpy as np
 
-from sympy import sympify, lambdify
+from sympy import sympify, lambdify, solve, solveset, Interval, Eq, latex, sin, cos, Matrix, re
 from sympy.abc import u, v
 
 import matplotlib.pyplot as plt
@@ -29,6 +29,12 @@ class MainWindow(QMainWindow):
         LabelXmax = QLabel("Xmax")
         LabelYmin = QLabel("Ymin")
         LabelYmax = QLabel("Ymax")
+        LabelNullclines = QLabel("Nullclines")
+        LabelEquilibria = QLabel("Equilibria")
+
+        #Create checkbox for nullclines
+        self.NullclinesCheckbox = QCheckBox()
+        self.EquilibriaCheckbox = QCheckBox()
 
 
         #Create button for plotting
@@ -38,22 +44,21 @@ class MainWindow(QMainWindow):
         
         #Create input for F1
         self.InputF1 = QLineEdit()
-        self.InputF1.setText("cos(u)")
+        self.InputF1.setText("v-0.5*u")
 
         #Create input for F2
         self.InputF2 = QLineEdit()
-        self.InputF2.setText("sin(v)")
+        self.InputF2.setText("sin(u)")
 
         #Create input for Xmin, Xmax, Ymin, Ymax
         self.InputXmin = QLineEdit()
-        self.InputXmin.setText("0")
+        self.InputXmin.setText("-5")
         self.InputXmax = QLineEdit()
         self.InputXmax.setText("5")
         self.InputYmin = QLineEdit()
-        self.InputYmin.setText("0")
+        self.InputYmin.setText("-5")
         self.InputYmax = QLineEdit()
         self.InputYmax.setText("5")
-
 
         #Create canvas to display plot
         self.fig, self.ax = plt.subplots()
@@ -80,9 +85,6 @@ class MainWindow(QMainWindow):
 
         layout.addWidget(LabelF2, 1, 0)
         layout.addWidget(self.InputF2, 1, 1)
-
-        layout.addWidget(LabelDensity, 6, 0)
-        layout.addWidget(self.Slider, 6, 1)
         
         layout.addWidget(LabelXmin, 2, 0)
         layout.addWidget(self.InputXmin, 2, 1)
@@ -95,6 +97,15 @@ class MainWindow(QMainWindow):
 
         layout.addWidget(LabelYmax, 5, 0)
         layout.addWidget(self.InputYmax, 5, 1)
+
+        layout.addWidget(LabelDensity, 6, 0)
+        layout.addWidget(self.Slider, 6, 1)
+
+        layout.addWidget(LabelEquilibria, 7, 0)
+        layout.addWidget(self.EquilibriaCheckbox, 7, 1)
+        
+        layout.addWidget(LabelNullclines, 8, 0)
+        layout.addWidget(self.NullclinesCheckbox, 8, 1)
              
         layout.addWidget(PlotButton, 10, 0, 1, 2)
         layout.addWidget(self.canvas, 0, 2, 10, 1)
@@ -120,8 +131,13 @@ class MainWindow(QMainWindow):
 
         X, Y = np.meshgrid(x, y)
 
-        F1 = lambdify([u,v], sympify(F1))
-        F2 = lambdify([u,v], sympify(F2))
+        F1_sympy = sympify(F1)
+        F2_sympy = sympify(F2)
+
+
+        F1 = lambdify([u,v], F1_sympy)
+        F2 = lambdify([u,v], F2_sympy)
+
 
         U = F1(X, Y)
         V = F2(X, Y)
@@ -129,8 +145,125 @@ class MainWindow(QMainWindow):
         self.ax.clear()
         self.ax.set_xlabel("u")
         self.ax.set_ylabel("v")
+        self.ax.set_xlim(Xmin, Xmax)
+        self.ax.set_ylim(Ymin, Ymax)
         plt.tight_layout()
-        self.ax.streamplot(X, Y, U, V, density=self.Slider.value())
+        self.ax.streamplot(X, Y, U, V, density=self.Slider.value(), color="tab:gray")
+
+
+        plotColors = ["tab:blue", "tab:orange", "tab:green", "tab:red", "tab:purple"] #Add more colors to be sure
+
+
+        if self.EquilibriaCheckbox.isChecked():
+            SolutionSet = []
+
+            #Compute Jacobian matrix
+            F = Matrix([F1_sympy, F2_sympy])
+            J = F.jacobian(Matrix([u,v]))
+
+            if F1_sympy.has(cos) or F1_sympy.has(sin):
+                if F2_sympy.has(cos) or F2_sympy.has(sin):
+                    print("double trig functions")
+                else:
+                    F1_symbols = F1_sympy.free_symbols
+                    if len(F1_symbols) == 2:
+                        sol = solveset(F1_sympy, (u,v))
+                        print("How to handle this?")
+                    elif len(F1_symbols) == 1:
+                        if u in F1_symbols:
+                            sol1 = solveset(F1_sympy, u, domain=Interval(Xmin, Xmax))
+                            for s in sol1:
+                                sol = solve(F2_sympy.subs(u, s))
+                                for s2 in sol:
+                                    SolutionSet.append([s, s2])
+                        else:
+                            sol1 = solveset(F1_sympy, v, domain=Interval(Ymin, Ymax))
+                            for s in sol1:
+                                sol = solve(F2_sympy.subs(v, s))
+                                for s2 in sol:
+                                    SolutionSet.append([s2, s])
+                    else: print("Not interesting")
+            elif F2_sympy.has(cos) or F2_sympy.has(sin):
+                if F1_sympy.has(cos) or F1_sympy.has(sin):
+                    print("double trig functions, but now handled by the above")
+                else:
+                    F2_symbols = F2_sympy.free_symbols
+                    if len(F2_symbols) == 2:
+                        sol = solve(F2_sympy, [u,v])
+                        print(sol)
+                        print("How to handle this?")
+                    elif len(F2_symbols) == 1:
+                        if u in F2_symbols:
+                            sol1 = solveset(F2_sympy, u, domain=Interval(Xmin, Xmax))
+                            for s in sol1:
+                                sol = solve(F1_sympy.subs(u, s))
+                                for s2 in sol:
+                                    SolutionSet.append([s, s2])
+                        else:
+                            sol1 = solveset(F2_sympy, v, domain=Interval(Ymin, Ymax))
+                            for s in sol1:
+                                sol = solve(F1_sympy.subs(v, s))
+                                for s2 in sol:
+                                    SolutionSet.append([s2, s])
+                    else: print("Not interesting")
+            else:
+                sol = solve([F1_sympy, F2_sympy], [u,v])
+            
+            for eq in SolutionSet:
+                J_eq = J.subs([(u, eq[0]), (v, eq[1])])
+                ev_J = list(J_eq.eigenvals().keys())
+                re_ev_J = np.array([re(i) for i in ev_J])
+                if len(re_ev_J[re_ev_J>0])>0:
+                    self.ax.scatter(eq[0], eq[1], marker = 'o', s=50, color="black", zorder=10)
+                    self.ax.scatter(eq[0], eq[1], marker = 'o', s=20, color="white", zorder=10)
+                else:
+                    self.ax.scatter(eq[0], eq[1], marker = 'o', s=50, color="black", zorder=10)
+
+
+        if self.NullclinesCheckbox.isChecked():
+            solCounter = 0
+            for F in [F1_sympy, F2_sympy]:
+                F_symbols = F.free_symbols
+                if F.has(cos) or F.has(sin):
+                    if len(F_symbols) == 2:
+                        F_sol = solveset(F, v)
+                        for sol in F_sol:
+                            F_sol_num = lambdify(u, sol, "numpy")
+                            self.ax.plot(x, F_sol_num(x), c=plotColors[solCounter], label=r"$v = " + latex(sol) + r"$")
+                            solCounter+=1
+                    elif len(F_symbols) == 1:
+                        if u in F_symbols:
+                            F_sol = solveset(F, u, domain=Interval(Xmin, Xmax))
+                            for sol in F_sol:
+                                self.ax.vlines(sol, Ymin, Ymax, color=plotColors[solCounter], label=r"$u = " + latex(sol) + r"$")
+                                solCounter+=1
+                        else:
+                            F_sol = solveset(F, v, domain=Interval(Ymin, Ymax))
+                            for sol in F_sol:
+                                self.ax.hlines(sol, Xmin, Xmax, color=plotColors[solCounter], label=r"$v = " + latex(sol) + r"$")
+                                solCounter+=1
+                    else: print("Not interesting")
+                else:
+                    if len(F_symbols) == 2:
+                        F_sol = solve(F, v)
+                        for sol in F_sol:
+                            F_sol_num = lambdify(u, sol, "numpy")
+                            self.ax.plot(x, F_sol_num(x), c=plotColors[solCounter], label=r"$v = " + latex(sol) + r"$")
+                            solCounter+=1
+                    elif len(F_symbols) == 1:
+                        if u in F_symbols:
+                            F_sol = solve(F, u, domain=Interval(Xmin, Xmax))
+                            for sol in F_sol:
+                                self.ax.vlines(sol, Ymin, Ymax, color=plotColors[solCounter], label=r"$u = " + latex(sol) + r"$")
+                                solCounter+=1
+                        else:
+                            F_sol = solve(F, v, domain=Interval(Ymin, Ymax))
+                            for sol in F_sol:
+                                self.ax.hlines(sol, Xmin, Xmax, color=plotColors[solCounter], label=r"$v = " + latex(sol) + r"$")
+                                solCounter+=1
+                    else: print("Not interesting")
+            self.ax.legend()
+        
         self.canvas.draw()
 
 
